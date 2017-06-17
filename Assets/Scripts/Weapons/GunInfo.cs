3779345b4b40fcb8f10bf13954db6d15cb7af8fd
Christian Gonzalez <aushou@me.com>
2017-06-17 09:39:36 -0400

    feat(weapons): Add new stats to guns Add max fire rate Add automatic

--- Assets/Scripts/Weapons/GunInfo.cs
+++ Assets/Scripts/Weapons/GunInfo.cs
@@ -18,10 +18,17 @@ public class GunInfo {
 	private float maxJitterAngle = -1;
 	[SerializeField]
 	private string slug = "uninitialized_gun";
+	[SerializeField]
+	private float maxFireRate = -1;
+	// TODO: Convert this to a firetype enum
+	[SerializeField]
+	private bool isAutomatic = false;
 
 	public string Name { get { return name; } }
 	public string Description { get { return description; } }
 	public string Slug { get { return slug; } }
 	public float MuzzleVelocity { get { return muzzleVelocity; } }
 	public float MaxJitterAngle { get { return maxJitterAngle; } }
+	public float MaxFireRate { get { return maxFireRate; } }
+	public bool IsAustomatic { get { return isAutomatic; } }
 }
